de345e4ab36f9bc9742e10da7b435f5b406bd114
Jean Gomes <88164101+jeangomes13@users.noreply.github.com>
2022-07-12 18:41:15 -0300

    Innout-início

---
+++ innout/src/views/template/messages.php
@@ -0,0 +1,22 @@
+<?php
+if ($exception) {
+   $message = [
+      'type' => 'error',
+      'message' => $exception->getMessage()
+   ];
+}
+
+$alertType = '';
+
+if($message['type'] === 'error') {
+   $alertType =  'danger';
+} else {
+   $alertType = 'sucess';
+}
+?>
+<?php if ($message) : ?>
+   <div role="alert"
+        class="my-3 alert alert-<?= $alertType ?>" >
+        <?= $message['message'] ?>
+   </div>
+<?php endif ?>
